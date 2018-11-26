67b13db760a64479e57b31da1d0e4269fa230d1e
Joyce Liao <joyliao07@gmail.com>
2018-11-26 15:19:26 -0800

    complete menu print

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -5,6 +5,7 @@ WIDTH = 100
 line_one = 'Welcome to Snake Cafe!'
 line_two = 'Please see our menu below.'
 line_three = 'To quit any time, type "quit".'
+line_four = 'What would you like to order?'
 
 
 print(dedent(f'''
@@ -14,4 +15,50 @@ print(dedent(f'''
     {'**'}
     {('**' + (' ' * ((WIDTH - len('**')*2 - len(line_three))//2 )) + line_three + (' ' * ((WIDTH - len('**')*2 - len(line_three))//2)) + '**')}
     {('*' * WIDTH)}
+    {(' ' * WIDTH)}
+
+    {'Appetizers'}
+    {('-' * len('Appetizers'))}
+    {'Wings'}
+    {'Cookies'}
+    {'Spring Rolls'}
+    {(' ' * WIDTH)}
+
+    {'Entrees'}
+    {('-' * len('Entrees'))}
+    {'Salmon'}
+    {'Steak'}
+    {'Meat Tornado'}
+    {'A Literal Garden'}
+    {(' ' * WIDTH)}
+
+    {'Desserts'}
+    {('-' * len('Desserts'))}
+    {'Ice Cream'}
+    {'Cake'}
+    {'Pie'}
+    {(' ' * WIDTH)}
+
+    {'Drinks'}
+    {('-' * len('Drinks'))}
+    {'Coffee'}
+    {'Tea'}
+    {'Blood of the Innocent'}
+    {(' ' * WIDTH)}
+
+    {('*' * WIDTH)}
+    {('**' + (' ' * ((WIDTH - len('**')*2 - len(line_four))//2 )) + line_four + (' ' * ((WIDTH - len('**')*2 - len(line_four))//2 )) + '**')}
+    {('*' * WIDTH)}
 '''))
+
+
+
+
+
+
+
+
+
+
+
+
