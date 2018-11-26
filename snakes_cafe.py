ea319ca2d657f4a2661573ee51a419ad329f348a
Joyce Liao <joyliao07@gmail.com>
2018-11-26 15:08:31 -0800

    welcome line complete

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -4,11 +4,14 @@ import sys
 WIDTH = 100
 line_one = 'Welcome to Snake Cafe!'
 line_two = 'Please see our menu below.'
-line_three = 'To quit any time, type "quit"'
+line_three = 'To quit any time, type "quit".'
 
 
 print(dedent(f'''
     {('*' * WIDTH)}
-    {('**' + (' ' * ((WIDTH - len('**') - len(line_one))//2 )) + line_one + (' ' * 5) + '**')}
-
+    {('**' + (' ' * ((WIDTH - len('**')*2 - len(line_one))//2 )) + line_one + (' ' * ((WIDTH - len('**')*2 - len(line_one))//2 )) + '**')}
+    {('**' + (' ' * ((WIDTH - len('**')*2 - len(line_two))//2 )) + line_two + (' ' * ((WIDTH - len('**')*2 - len(line_two))//2 )) + '**')}
+    {'**'}
+    {('**' + (' ' * ((WIDTH - len('**')*2 - len(line_three))//2 )) + line_three + (' ' * ((WIDTH - len('**')*2 - len(line_three))//2)) + '**')}
+    {('*' * WIDTH)}
 '''))
